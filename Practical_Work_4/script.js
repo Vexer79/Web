797d79e8aced3f6e30e7ad24e505597906b3c32d
Vexer79 <rokytskyi.i@gmail.com>
2024-02-20 15:12:36 +0200

    feat: add TestAllFunctions function

--- Practical_Work_4/script.js
+++ Practical_Work_4/script.js
@@ -16,3 +16,15 @@ function MyMathPowerDefault1(b, n) {
 function MyMathPowerDefault2(b = 2, n = 3){
     return MyMathPower(b, n);
 }
+
+function TestAllFunctions(){
+    MyMathPowerDefault1(3, 3);
+    MyMathPowerDefault1(3);
+    MyMathPowerDefault1(null, 3);
+    MyMathPowerDefault1();
+    MyMathPowerDefault2(4, 4);
+    MyMathPowerDefault2(4);
+    MyMathPowerDefault2(null, 4);
+    MyMathPowerDefault2();
+}
+TestAllFunctions();
