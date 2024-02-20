3660e2b3d20c5b01f568c0d00b9e6eac48add90d
Vexer79 <rokytskyi.i@gmail.com>
2024-02-20 15:13:15 +0200

    refactor: add enters and spaces

--- Practical_Work_4/script.js
+++ Practical_Work_4/script.js
@@ -13,11 +13,11 @@ function MyMathPowerDefault1(b, n) {
     return MyMathPower(b, n);
 }
 
-function MyMathPowerDefault2(b = 2, n = 3){
+function MyMathPowerDefault2(b = 2, n = 3) {
     return MyMathPower(b, n);
 }
 
-function TestAllFunctions(){
+function TestAllFunctions() {
     MyMathPowerDefault1(3, 3);
     MyMathPowerDefault1(3);
     MyMathPowerDefault1(null, 3);
