b75d641a5fd9048ea0aeb04398ea63a3a51615d1
Vexer79 <rokytskyi.i@gmail.com>
2024-02-20 15:00:09 +0200

    feat: add MyMathPowerDefault1 function

--- Practical_Work_4/script.js
+++ Practical_Work_4/script.js
@@ -6,3 +6,9 @@ function MyMathPower(b, n) {
     console.log(`The ${n}th power of ${b} equals ${r}`);
     return r;
 }
+
+function MyMathPowerDefault1(b, n) {
+    b = b || 2;
+    n = n || 3;
+    return MyMathPower(b, n);
+}
