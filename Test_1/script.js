bc8da172c59cb811ea57bb878f4a25d526ceaf17
Vexer79 <rokytskyi.i@gmail.com>
2024-03-15 11:01:13 +0200

    feat: add non ES6 constructors

--- Test_1/script.js
+++ Test_1/script.js
@@ -5,3 +5,21 @@ function TriangleArea(a = 7, h_a = 3) {
 
 TriangleArea(3, 6);
 TriangleArea();
+
+function Jet() {
+    this.color = "orange";
+    this.avgSpeed = 320;
+    this["max tonnage"] = 20;
+    this.brand = "AirBus";
+    this["country of registration"] = "Poland";
+}
+
+Jet.prototype.AssignCaptain = function () {
+    this.name = "Ivan Rokytskyi";
+    this["years of experience"] = 4;
+    this.hasFamily = true;
+}
+let jet = new Jet();
+console.log(jet);
+jet.AssignCaptain();
+console.log(jet);
