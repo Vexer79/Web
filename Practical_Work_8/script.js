b0d1bbd803d85124fd736ede145f8736a4dcdb2f
Vexer79 <rokytskyi.i@gmail.com>
2024-03-19 14:52:24 +0200

    feat: improve algorithm

--- Practical_Work_8/script.js
+++ Practical_Work_8/script.js
@@ -2,7 +2,7 @@
     var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
     for (let name of names) {
-        if (name.charAt(0) == "j" || name.charAt(0) == "J") {
+        if (name.charAt(0).toLowerCase() == "j") {
             speakHello.speak(name);
         } else {
             speakGoodBye.speak(name);
