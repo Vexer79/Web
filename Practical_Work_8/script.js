490679cc0da7cae9d1a64a9b193f22dd1d6c0dab
Vexer79 <rokytskyi.i@gmail.com>
2024-03-19 14:49:09 +0200

    feat: add functions speakHello and speakGoodBye to script.js

--- Practical_Work_8/script.js
+++ Practical_Work_8/script.js
@@ -1,11 +1,11 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+    var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-for () {
-
-  
-  if () {
-    
-  } else {
-    
-  }
-}
+    for (let name of names) {
+        if (name.charAt(0) == "j" || name.charAt(0) == "J") {
+            speakHello.speak(name);
+        } else {
+            speakGoodBye.speak(name);
+        }
+    }
+})();
