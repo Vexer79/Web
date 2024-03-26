bd167cd4fdbc647f0859ff54a20a461dd1e86c73
Vexer79 <rokytskyi.i@gmail.com>
2024-03-26 12:41:04 +0200

    feat: add array

---
+++ Practical_Work_9/script.js
@@ -0,0 +1,16 @@
+//1.2.3
+const arr1 = [
+    1,
+    5,
+    9,
+    4,
+    { name: "Ivan", mark: -1, subject: "JS" },
+    function (number) {
+        return number ** 4;
+    },
+    function (object) {
+        console.log(object.name);
+    },
+    -6,
+    14,
+];
