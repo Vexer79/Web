387d8e6b157c8f715f48905c60043e5e8cf1a5ed
Vexer79 <rokytskyi.i@gmail.com>
2024-03-28 23:02:25 +0200

    feat: add selection sort

--- Practical_Work_10/sort-library.js
+++ Practical_Work_10/sort-library.js
@@ -13,10 +13,29 @@
                 }
             }
         }
-        return (reversed) ? array.reverse() : array;
+        return reversed ? array.reverse() : array;
     }
 
     console.log(bubbleSort([1, 3, 6, 3, 1, 3, 5], true));
+
+    function selectionSort(array, reversed) {
+        for (let index = 0; index < array.length - 1; index++) {
+            let min = index;
+            for (let current = index + 1; current < array.length; current++) {
+                if (array[current] < array[min]) {
+                    min = current;
+                }
+            }
+            let tempElem = array[index];
+            array[index] = array[min];
+            array[min] = tempElem;
+        }
+
+        return reversed ? array.reverse() : array;
+    }
+
+    console.log(selectionSort([1, 3, 6, 3, 1, 3, 5]));
+
 
     global.SortLibrary = SortLibrary;
 })(window);
