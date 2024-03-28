477783e22bc21cbdf034195de0a0c50b069c9729
Vexer79 <rokytskyi.i@gmail.com>
2024-03-28 22:54:14 +0200

    feat: add bubble sort prototype

--- Practical_Work_10/sort-library.js
+++ Practical_Work_10/sort-library.js
@@ -2,8 +2,21 @@
     const SortLibrary = {};
 
     function bubbleSort(array, reversed) {
-        return reversed ? array : array.reversed();
+        while (!isSorted(array)) {
+            for (let index = 0; index < array.length - 1; index++) {
+                if (array[index] > array[index + 1]) {
+                    let tempElem = array[index];
+                    array[index] = array[index + 1];
+                    array[index + 1] = tempElem;
+                }
+            }
+        }
+        return (reversed) ? array.reverse() : array;
     }
+
+    const isSorted = (arr) => arr.every((value, index, item) => !index || item[index - 1] <= value);
+
+    console.log(bubbleSort([1, 3, 6, 3, 1, 3, 5], true));
 
     global.SortLibrary = SortLibrary;
 })(window);
