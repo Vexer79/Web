914e62d9684deffc703456181e7becc4b9362a71
Vexer79 <rokytskyi.i@gmail.com>
2024-03-29 08:22:28 +0200

    feat: replace array.reverse() to compare function

--- Practical_Work_10/sort-library.js
+++ Practical_Work_10/sort-library.js
@@ -1,28 +1,37 @@
 (function (global) {
     const SortLibrary = {};
 
-    const isSorted = (arr) => arr.every((value, index, item) => !index || item[index - 1] <= value);
+    const isSorted = function (array, reversed) {
+        return reversed
+            ? array.every((value, index, item) => !index || item[index - 1] >= value)
+            : arr.every((value, index, item) => !index || item[index - 1] <= value);
+    };
+    function getCompare(reversed) {
+        return reversed ? (left, right) => left < right : (left, right) => left > right;
+    }
 
     function bubbleSort(array, reversed) {
-        while (!isSorted(array)) {
+        const compare = getCompare(reversed);
+        while (!isSorted(array, reversed)) {
             for (let index = 0; index < array.length - 1; index++) {
-                if (array[index] > array[index + 1]) {
+                if (compare(array[index], array[index + 1])) {
                     let tempElem = array[index];
                     array[index] = array[index + 1];
                     array[index + 1] = tempElem;
                 }
             }
         }
-        return reversed ? array.reverse() : array;
+        return array;
     }
 
     console.log(bubbleSort([1, 3, 6, 3, 1, 3, 5], true));
 
     function selectionSort(array, reversed) {
+        const compare = getCompare(!reversed);
         for (let index = 0; index < array.length - 1; index++) {
             let min = index;
             for (let current = index + 1; current < array.length; current++) {
-                if (array[current] < array[min]) {
+                if (compare(array[current], array[min])) {
                     min = current;
                 }
             }
@@ -31,23 +40,23 @@
             array[min] = tempElem;
         }
 
-        return reversed ? array.reverse() : array;
+        return array;
     }
 
     console.log(selectionSort([1, 3, 6, 3, 1, 3, 5]));
 
     function insertionSort(array, reversed) {
+        const compare = getCompare(reversed);
         for (let index = 1; index < array.length; index++) {
             let left = index - 1;
-            while (left > -1 && array[left] > array[left + 1]) {
+            while (left > -1 && compare(array[left], array[left + 1])) {
                 let tempElem = array[left + 1];
                 array[left + 1] = array[left];
                 array[left] = tempElem;
                 left--;
             }
         }
-
-        return reversed ? array.reverse() : array;
+        return array;
     }
 
     console.log(insertionSort([6, 5, 4, 3, 2, 1]));
