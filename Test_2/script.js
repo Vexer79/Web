d7ec52418b3f88ada2458774f622e9a2ac3acee8
Vexer79 <rokytskyi.i@gmail.com>
2024-04-09 12:56:01 +0300

    feat: add event on click to generate-button

--- Test_2/script.js
+++ Test_2/script.js
@@ -15,5 +15,20 @@
     let array = generateArray(20);
     console.log(SortLibrary.studentyEvakyjytsaPidChasMinuvannyaSort(array, true));
     document.getElementById("container").textContent = array;
-    
+    document.getElementById("generate-button").addEventListener("click", (event) => {
+        if (!document.getElementById("select-container")) {
+            createContainer();
+        } else {
+            document.getElementById("select-container").remove();
+            createContainer();
+        }
+    });
+    function createContainer() {
+        const div = document.createElement("div");
+        div.id = "select-container";
+        div.style.backgroundColor = "blue";
+        div.style.width = "30px";
+        div.style.height = `${document.getElementById("height").value}px`;
+        document.body.appendChild(div);
+    }
 })(window);
