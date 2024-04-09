a7e89a633845e0cb0f781b98315a241eb650b765
Vexer79 <rokytskyi.i@gmail.com>
2024-04-09 13:32:32 +0300

    feat: add buttons to move container

--- Test_2/script.js
+++ Test_2/script.js
@@ -16,10 +16,16 @@
     console.log(SortLibrary.studentyEvakyjytsaPidChasMinuvannyaSort(array, true));
     document.getElementById("container").textContent = array;
     document.getElementById("generate-button").addEventListener("click", (event) => {
-        if (!document.getElementById("select-container")) {
+        console.log();
+        if (
+            !document.getElementById("select-container") &&
+            document.getElementById("height").value
+        ) {
             createContainer();
         } else {
             document.getElementById("select-container").remove();
+            document.getElementById("left").remove();
+            document.getElementById("right").remove();
             createContainer();
         }
     });
@@ -27,8 +33,34 @@
         const div = document.createElement("div");
         div.id = "select-container";
         div.style.backgroundColor = "blue";
+        div.style.position = "relative";
         div.style.width = "30px";
         div.style.height = `${document.getElementById("height").value}px`;
         document.body.appendChild(div);
+        createButtons();
+    }
+    function createButtons() {
+        const buttonLeft = document.createElement("button");
+        const buttonRight = document.createElement("button");
+        buttonLeft.id = "left";
+        buttonRight.id = "right";
+        buttonLeft.textContent = "left";
+        buttonRight.textContent = "right";
+
+        buttonLeft.addEventListener("click", (event) => {
+            let leftVal = document.getElementById("select-container").style.left;
+            leftVal = leftVal.replace("px", "");
+            leftVal = Number(leftVal) - 20;
+            document.getElementById("select-container").style.left = `${leftVal}px`;
+        });
+        buttonRight.addEventListener("click", (event) => {
+            let rightVal = document.getElementById("select-container").style.left;
+            rightVal = rightVal.replace("px", "");
+            rightVal = Number(rightVal) + 20;
+            document.getElementById("select-container").style.left = `${rightVal}px`;
+        });
+
+        document.body.appendChild(buttonLeft);
+        document.body.appendChild(buttonRight);
     }
 })(window);
