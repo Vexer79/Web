33e05a9e8a928ee7917a07a9df24b5854d72ab93
Vexer79 <rokytskyi.i@gmail.com>
2024-04-09 12:44:16 +0300

    feat: generate and sort array with random numbers

--- Test_2/script.js
+++ Test_2/script.js
@@ -1,3 +1,17 @@
+//Варіант 2
 (function (global) {
-    
+    function getRandomInt(max = 100) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function generateArray(n) {
+        let array = [];
+        for (let index = 0; index < n; index++) {
+            array[index] = getRandomInt();
+        }
+        return array;
+    }
+
+    let array = generateArray(20);
+    console.log(SortLibrary.studentyEvakyjytsaPidChasMinuvannyaSort(array, true));
 })(window);
