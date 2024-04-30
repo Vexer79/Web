a0bb50fdfe8f411a1a58376401f68d0c54a7f8d0
Vexer79 <rokytskyi.i@gmail.com>
2024-05-01 00:54:36 +0300

    feat: add snippet loading script to script.js

--- Practical_Work_19/js/script.js
+++ Practical_Work_19/js/script.js
@@ -1,1 +1,35 @@
-import "./burger.js"
+import "./burger.js";
+import "./ajax-utils.js";
+import carousel from "./carousel.js";
+
+(function (global) {
+    let Content = {};
+    const homeHTML = "./snippets/home-snippet.html";
+    const contentContainerSelector = ".content__container";
+
+    const insertHTML = (selector, html) => {
+        document.querySelector(selector).innerHTML = html;
+    };
+
+    const showLoading = (selector) => {
+        document.querySelector(selector).innerHTML = `
+                <div class="loader__container">
+                    <div class="loader"></div>
+                </div>`;
+    };
+
+    document.addEventListener("DOMContentLoaded", (event) => {
+        showLoading(contentContainerSelector);
+        setTimeout(() => {
+            ajaxUtils.sendGetRequest(
+                homeHTML,
+                (response) => {
+                    insertHTML(contentContainerSelector, response);
+                    carousel();
+                },
+                false
+            );
+        }, 3000);
+    });
+    global.Content = Content;
+})(window);
