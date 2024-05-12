15317eee29cd01e31423932aa862d2671c19a94d
Vexer79 <rokytskyi.i@gmail.com>
2024-05-12 14:20:23 +0200

    feat: add ability to load catalog items

--- Practical_Work_21/js/script.js
+++ Practical_Work_21/js/script.js
@@ -13,8 +13,103 @@ import carousel from "./carousel.js";
     const allCategoriesUrl = "data/categories.json";
     const categoriesTitleHtml = "snippets/categories-title-snippet.html";
     const categoryHtml = "snippets/category-snippet.html";
-    
-    
+
+    const catalogItemsUrl = "data/catalog/";
+    const catalogItemsTitleHtml = "snippets/catalog-items-title.html";
+    const catalogItemHtml = "snippets/catalog-item.html";
+
+    Content.loadCatalogItems = function (categoryShort) {
+        if (!isLoading) {
+            isLoading = true;
+            showLoading(contentContainerSelector);
+            ajaxUtils.sendGetRequest(`${catalogItemsUrl}${categoryShort}.json`, (response) => {
+                buildAndShowCatalogItemsHTML(response);
+                isLoading = false;
+            });
+        }
+    };
+
+    function buildAndShowCatalogItemsHTML(categoryCatalogItems) {
+        ajaxUtils.sendGetRequest(
+            catalogItemsTitleHtml,
+            (categoriesTitleHtml) => {
+                ajaxUtils.sendGetRequest(
+                    catalogItemHtml,
+                    (categoryHtmlTemplate) => {
+                        const { html, catalogItemsTitleHtml } = buildCatalogItemsViewHtml(
+                            categoryCatalogItems,
+                            categoriesTitleHtml,
+                            categoryHtmlTemplate
+                        );
+                        insertHTML(catalogHeaderContainer, catalogItemsTitleHtml);
+                        insertHTML(contentContainerSelector, html);
+                    },
+                    false
+                );
+            },
+            false
+        );
+    }
+
+    function buildCatalogItemsViewHtml(
+        categoryCatalogItems,
+        catalogItemsTitleHtml,
+        catalogItemHtml
+    ) {
+        let html = "<div class='main-catalog__container'>";
+        for (const categoryCatalogItem of categoryCatalogItems.content) {
+            let itemHtml = catalogItemHtml;
+            itemHtml = insertProperty(itemHtml, "id", categoryCatalogItem.id);
+            itemHtml = insertProperty(itemHtml, "title", categoryCatalogItem.title);
+            itemHtml = insertProperty(itemHtml, "description", categoryCatalogItem.description);
+            itemHtml = insertProperty(itemHtml, "url", categoryCatalogItem["img-url"]);
+            itemHtml = insertItemPrice(
+                itemHtml,
+                "max-price",
+                categoryCatalogItem.price["max-price"]
+            );
+            itemHtml = insertItemPrice(
+                itemHtml,
+                "min-price",
+                categoryCatalogItem.price["min-price"]
+            );
+            itemHtml = insertItemAmount(
+                itemHtml,
+                "max-amount",
+                categoryCatalogItem.price["max-amount"]
+            );
+            html += itemHtml;
+        }
+        html += "</div>";
+        catalogItemsTitleHtml = insertProperty(
+            catalogItemsTitleHtml,
+            "name",
+            categoryCatalogItems.header.name
+        );
+        catalogItemsTitleHtml = insertProperty(
+            catalogItemsTitleHtml,
+            "description",
+            categoryCatalogItems.header.description
+        );
+        return { html, catalogItemsTitleHtml };
+    }
+
+    function insertItemPrice(html, pricePropName, priceValue) {
+        if (priceValue === undefined) {
+            return insertProperty(html, pricePropName, ``);
+        } else {
+            return insertProperty(html, pricePropName, `$${priceValue.toFixed(2)}`);
+        }
+    }
+
+    function insertItemAmount(html, amountPropName, amountPropValue) {
+        if (amountPropValue === undefined) {
+            return insertProperty(html, amountPropName, ``);
+        } else {
+            return insertProperty(html, amountPropName, `(${amountPropValue}+pos)`);
+        }
+    }
+
     //Previous practical work
     catalogLink.addEventListener("click", (event) => {
         Content.loadCatalogCategories();
@@ -50,6 +145,15 @@ import carousel from "./carousel.js";
                         );
                         insertHTML(catalogHeaderContainer, categoriesTitle);
                         insertHTML(contentContainerSelector, html);
+                        for (const category of document.querySelectorAll(
+                            ".catalog-item__container"
+                        )) {
+                            category.addEventListener("click", (event) => {
+                                Content.loadCatalogItems(
+                                    category.children[category.children.length - 1].textContent
+                                );
+                            });
+                        }
                     },
                     false
                 );
@@ -61,7 +165,6 @@ import carousel from "./carousel.js";
     function buildCategoriesViewHtml(categories, categoriesTitle, categoryHtmlTemplate) {
         let html = "<div class='main-catalog__container'>";
         for (const category of categories) {
-            console.log(category);
             let categoryHtml = categoryHtmlTemplate;
             categoryHtml = insertProperty(categoryHtml, "short_name", category["img-url"]);
             categoryHtml = insertProperty(categoryHtml, "name", category.title);
@@ -99,7 +202,7 @@ import carousel from "./carousel.js";
                 false
             );
             isLoading = false;
-        }, 3750);
+        }, 1);
     });
     global.Content = Content;
 })(window);
